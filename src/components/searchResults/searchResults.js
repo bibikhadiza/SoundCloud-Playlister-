76071975ff89f33a0464b27d0f11ac4484a55a28
Bibi Khadiza <limakhadiza@yahoo.com>
2017-04-14 06:06:19 -0400

    intergrated links to user page

--- src/components/searchResults/searchResults.js
+++ src/components/searchResults/searchResults.js
@@ -12,7 +12,7 @@ const SearchResults = ({ results, changeTrack }) => {
       image = "https://www.stjordalfoto.no/templates/newyork/images/no_image.png"
     }
 
-    return <div className="ui card" onSubmit={changeTrack} key={i}><div className="image"><img src={image} alt=""/></div><div className="content"><div className="meta"><span className="date">{e.user.username}</span></div><div className="description">{e.title}</div></div><div className="extra content"><form onSubmit={changeTrack} id={e.id} name={e.title}><input type="submit" value="play" className="ui button" /></form></div></div>
+    return <div className="ui card" onSubmit={changeTrack} key={i}><div className="image"><img src={image} alt=""/></div><div className="content"><div className="meta"><span className="date"><a href={e.user.permalink_url} target="_blank">{e.user.username}</a></span></div><div className="description"><a href={e.permalink_url} target="_blank">{e.title}</a></div></div><div className="extra content"><form onSubmit={changeTrack} id={e.id} name={e.title}><input type="submit" value="play" className="ui button" /></form></div></div>
   })
 
   return(
