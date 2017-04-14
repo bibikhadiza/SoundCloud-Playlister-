3dc98397c945577e77a91a01ff90f942b73025c2
Bibi Khadiza <limakhadiza@yahoo.com>
2017-04-14 00:35:31 -0400

    header css completed

--- src/utils/search/search.js
+++ src/utils/search/search.js
@@ -5,9 +5,16 @@ import './search.css';
 
 
 const SearchResults = ({ results, changeTrack }) => {
-
   const result = results.map((e) => {
-    return <div className="ui card" onSubmit={changeTrack}><div className="image"><img src={e.artwork_url}/></div><div className="content"><div className="meta"><span className="date">{e.user.username}</span></div><div className="description">{e.title}</div></div><div className="extra content"><form onSubmit={changeTrack} id={e.id} name={e.title}><input type="submit" value="play" className="ui button" /></form></div></div>
+
+    let image = null;
+    if (e.artwork_url) {
+      image = e.artwork_url;
+    } else {
+      image = "https://www.stjordalfoto.no/templates/newyork/images/no_image.png"
+    } 
+
+    return <div className="ui card" onSubmit={changeTrack}><div className="image"><img src={image}/></div><div className="content"><div className="meta"><span className="date">{e.user.username}</span></div><div className="description">{e.title}</div></div><div className="extra content"><form onSubmit={changeTrack} id={e.id} name={e.title}><input type="submit" value="play" className="ui button" /></form></div></div>
   })
 
   return(<div className="track_cards">
